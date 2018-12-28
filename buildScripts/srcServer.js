fab93b0d35d291804e55c57311605437f67fa7b5
Nate Greenwood <nate.greenwood@impartner.com>
2018-12-28 13:48:52 -0700

    Checkpoint

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
 const port = 3000;
+const ltSubDomain = 'ngreenwood';
 const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+}))
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
@@ -14,5 +23,6 @@ app.listen(port, function(err) {
 		console.log(err);
 	} else {
 		open('http://localhost:' + port);
+		//open(`http://${ltSubDomain}.localtunnel.me`);
 	}
 });
