2294000d1a405c40b10035a1491196231994ff8e
Nate Greenwood <nategreenwood222@msn.com>
2018-12-29 01:05:45 -0700

    Test stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import './index.css';
 import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`Course value: ${courseValue}`);
+var envStuff = {
+    courseValue: numeral(1000).format('$0,0.00'),
+    anArray: [1, 2, 3],
+    anObject: {
+        val1: 1,
+        val2: 2
+    },
+    aFunction: function() {
+        return this.courseValue;
+    }
+};
+
+export default envStuff;
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,7 @@
 import {expect} from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
+import data from './index.js';
 
 describe('The first test', () => {
 	it('should pass', () => {
@@ -18,5 +19,18 @@ describe('index.html', () => {
 			done();
 			window.close();
 		});
+	});
+});
+
+describe('index.js', () => {
+	it('envStuff', (done) => {
+		expect(data).to.not.be.null;
+		expect(data).to.not.be.undefined;
+		expect(data.courseValue).to.equal('$1,000.00');
+		expect(data.anArray[0]).to.equal(1);
+		expect(data.anObject.val1).to.equal(1);
+		expect(data.aFunction()).to.equal(data.courseValue);
+
+		done();
 	});
 });
